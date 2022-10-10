1fb4863f24a481e2ab2009e50fb5efbec0b0653d
unknown <mosesfat@gmail.com>
2022-10-10 15:05:08 +0800

    sidebar stuff

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 // import { Link } from 'react-router-dom'
 
 import { Navlinks } from '../components/Navlinks'
@@ -9,9 +9,27 @@ const Sidebar = () => {
 
     const [open, setOpen] = useState(false)
 
+    let menuRef = useRef() 
+
+    useEffect(() => { // i still need to learn more about useEffect xD
+        let handler = (event) => {
+            if(!menuRef.current.contains(event.target)) { // this checks the location of the event clicked (mousedown)
+                setOpen(false)
+            }
+        }
+        
+        document.addEventListener('mousedown', handler)  
+        
+        return () => {
+            document.removeEventListener('mousedown', handler)
+        }
+    })
+
     return (
         <div className="flex float-left relative">
-            <div className={`${open ? 'w-72' : 'w-20'} duration-300 h-screen bg-dark-purple fixed`}>
+            <div 
+            ref={menuRef}
+            className={`${open ? 'w-72' : 'w-20'} duration-300 h-screen bg-dark-purple fixed`}>
                 <img src={require('../assets/control.png')} 
                 alt="sidebar_pic" 
                 className={`absolute cursor-pointer rounded-full -right-4

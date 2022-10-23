7a03088de64ac2046939e2329c41f927194b4bda
unknown <mosesfat@gmail.com>
2022-10-23 15:48:52 +0800

    booking page and live mass page

--- src/pages/bookingPage.js
+++ src/pages/bookingPage.js
@@ -1,7 +1,38 @@
+
+/*
+    page will have a drop down option to choose what to book
+    (unsay ganahan ipa misa)    
+*/
+
+import { Dropdown } from "flowbite-react"
+
 function bookingPage () {
     return (
-        <div className="flex justify-center items-center">
-            <h2>booking page</h2>
+        <div className="flex flex-col justify-center items-center p-10">
+            <h2 className="text-5xl font-semibold">Booking Page</h2> 
+            <div className="mt-20">
+            <Dropdown 
+            style={{ background: '#4D82DF' }}
+            size="lg"
+            label="Choose the type of Prayer or Event you want to book"
+            className="font-semibold w-80 text-center border-y-slate-300 border-x-slate-300">
+                <Dropdown.Item>
+                    Wedding
+                </Dropdown.Item>
+                <Dropdown.Item>
+                    Baptismal
+                </Dropdown.Item>
+                <Dropdown.Item>
+                    Thanksgiving Prayer
+                </Dropdown.Item>
+                <Dropdown.Item>
+                    Prayer for the dead
+                </Dropdown.Item>
+                <Dropdown.Item>
+                    Fiesta Mass
+                </Dropdown.Item>
+            </Dropdown>
+            </div>
         </div>
     )
 }
--- src/pages/liveMassPage.js
+++ src/pages/liveMassPage.js
@@ -1,7 +1,28 @@
+import React, {  } from 'react'
+
+const GetData = () => {
+    fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCKUsvn9QGIwiX8fjCNmetWA&maxResults=3&order=date&key=AIzaSyDWV26jvrvKzoLMky6RLHJS8xELSvONIj8")
+    .then((result) => {
+        return result.json()
+    }).then((data) => {
+        console.log(data)
+        let videos = data.items
+        for(let video of videos){
+            console.log(video.snippet.title)
+        }
+    })
+}
+
+
 function liveMassPage () {
     return (
-        <div className="flex justify-center items-center">
-            <h2>live mass page</h2>
+        <div className="flex justify-center items-center p-10">
+            <h2 className="text-5xl font-semibold">Live Mass Recordings</h2>
+            <div>
+                {
+                    GetData()
+                }
+            </div>
         </div>
     )
 }
